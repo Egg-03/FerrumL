131b85f74ebd569e6ea7cbbd0977103a979d325b
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-04-11 14:49:40 +0530

    Win32_Processor added

--- src/com/ferruml/formatter/wmic/WMIC.java
+++ src/com/ferruml/formatter/wmic/WMIC.java
@@ -1,13 +1,96 @@
 package com.ferruml.formatter.wmic;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-public class WMIC {
+public class WMIC{
 	private WMIC() {
 		throw new IllegalStateException("Utility Class");
 	}
 	
-	public static String getValues(String WMIC_Class, String WMIC_Attribute) throws IOException {
-		return WMICFormat.accessrunCommand(WMIC_Class, WMIC_Attribute);
+	public static Map<String, String> get(String WMIC_Class, String WMIC_Attributes) throws IOException, IndexOutOfBoundsException {
+		
+		String[] command = {"cmd","/c", "wmic "+WMIC_Class+" get "+WMIC_Attributes+" /format:list"};
+		Process process = Runtime.getRuntime().exec(command);
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		
+		String currentLine;
+		
+		String key = "";
+		String value = "";
+		Map<String, String> property = new LinkedHashMap<>();
+		
+		while((currentLine=br.readLine())!=null)
+			if(!currentLine.isBlank() || !currentLine.isEmpty()) {
+				if(currentLine.contains("=")) {
+					key = currentLine.substring(0, currentLine.indexOf("=")).strip();
+					value = currentLine.substring(currentLine.indexOf("=")+1).strip();
+					property.put(key, value);
+				}
+				else {
+					value=value.concat(currentLine);
+					property.replace(key, value);
+				}
+			}
+		br.close();
+		return property;
+	}
+	
+	public static List<String> getID(String WMIC_Class, String Key) throws IOException, IndexOutOfBoundsException{
+		String[] command = {"cmd","/c", "wmic "+WMIC_Class+" get "+Key+" /format:list"};
+		Process process = Runtime.getRuntime().exec(command);
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		
+		String currentLine;
+		
+		String value = "";
+		List<String> ID = new ArrayList<>();
+		
+		while((currentLine=br.readLine())!=null)
+			if(!currentLine.isBlank() || !currentLine.isEmpty()) {
+				if(currentLine.contains("=")) {
+					value = currentLine.substring(currentLine.indexOf("=")+1).strip();
+					ID.add(value);
+				}
+				else {
+					value=value.concat(currentLine);
+					ID.add(ID.indexOf(ID.getLast()), value);
+				}
+			}
+		br.close();
+		return ID;
+	}
+	
+	public static Map<String, String> getWhere(String WMIC_Class, String Precedent, String Antecedent, String WMIC_Attributes) throws IOException, IndexOutOfBoundsException {
+		
+		String[] command = {"cmd","/c", "wmic "+WMIC_Class+" where "+Precedent+"="+"\""+Antecedent+"\""+" get "+WMIC_Attributes+" /format:list"};
+		Process process = Runtime.getRuntime().exec(command);
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		
+		String currentLine;
+		
+		String key = "";
+		String value = "";
+		Map<String, String> property = new LinkedHashMap<>();
+		
+		while((currentLine=br.readLine())!=null)
+			if(!currentLine.isBlank() || !currentLine.isEmpty()) {
+				if(currentLine.contains("=")) {
+					key = currentLine.substring(0, currentLine.indexOf("=")).strip();
+					value = currentLine.substring(currentLine.indexOf("=")+1).strip();
+					property.put(key, value);
+				}
+				else {
+					value=value.concat(currentLine);
+					property.replace(key, value);
+				}
+			}
+		br.close();
+		return property;
 	}
 }
---
+++ src/com/ferruml/system/hardware/Win32_Processor.java
@@ -0,0 +1,26 @@
+package com.ferruml.system.hardware;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.ferruml.formatter.wmic.WMIC;
+
+public class Win32_Processor{
+	static String alias = "cpu"; //alias for Win32_Processor
+	static String precedent = "DeviceID";
+	
+	private Win32_Processor() {
+		throw new IllegalStateException("Utility Class");
+	}
+	
+	public static List<String> getProcessorList() throws IOException, IndexOutOfBoundsException {
+		return WMIC.getID(alias, precedent);
+	}
+	
+	public static Map<String, String> getCurrentProcessor(String currentProcessorID) throws IOException, IndexOutOfBoundsException {
+		String attributes = "Name, NumberOfCores, ThreadCount, Manufacturer, AddressWidth, L2CacheSize, L3CacheSize, MaxClockSpeed, ExtClock, SocketDesignation, Version, Caption, Family, Stepping, VirtualizationFirmwareEnabled, ProcessorID";
+		return WMIC.getWhere(alias, precedent, currentProcessorID, attributes);
+	}
+}
+
