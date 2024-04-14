86f796007cec1260ecd195ed9a09e0d4eabe4c9a
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-04-14 13:36:44 +0530

    Win32_SoundDevice

---
+++ src/com/ferruml/system/hardware/Win32_SoundDevice.java
@@ -0,0 +1,23 @@
+package com.ferruml.system.hardware;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.ferruml.formatter.wmic.WMIC;
+
+public class Win32_SoundDevice {
+	private static String classname = "Win32_SoundDevice";
+	private static String attributes = "ProductName, Status, Caption, PNPDeviceID, Manufacturer";
+	private Win32_SoundDevice() {
+		throw new IllegalStateException("Utility Class");
+	}
+	
+	public static List<String> getSoundDeviceID() throws IOException, IndexOutOfBoundsException {
+		return WMIC.getID(classname, "Caption"); //fallback to Caption since DeviceID does not work
+	}
+	
+	public static Map<String, String> getCurrentAudioDevice(String deviceID) throws IOException, IndexOutOfBoundsException {
+		return WMIC.getWhere(classname, "Caption", deviceID, attributes);
+	}
+}
